71a382b34bb751c94143494d814cdebb0c742009
dididrk <104390990+dididrk@users.noreply.github.com>
2022-05-14 03:51:00 +0100

    Update miller_rabin.cpp

--- miller_rabin.cpp
+++ miller_rabin.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <cmath>
 #include <random>
 #include <chrono>
 #include <omp.h>
@@ -12,9 +13,9 @@ std::uniform_real_distribution<double> dist;
 
 // ------------------------------------------------------------------------------------
 unsigned int size_initial_sieve = 10;
-unsigned int size_sufficient_witnesses = 3;
+unsigned int size_sufficient_witnesses = 7;
 unsigned long long initial_sieve[10] = {0};
-unsigned long long sufficient_witnesses[3] = {2, 7, 61}; // for n < 4,759,123,141
+unsigned long long sufficient_witnesses[7] = {2, 3, 5, 7, 11, 13, 17}; // for n < 341,550,071,728,321
 //unsigned long long count = 3 + size_initial_sieve;
 
 unsigned long long fastExp(unsigned long long b, unsigned long long e, unsigned long long m)
@@ -32,7 +33,7 @@ unsigned long long fastExp(unsigned long long b, unsigned long long e, unsigned 
 	return result;
 }
 
-unsigned long long miller_rabin(int n, int confidence) {
+unsigned long long miller_rabin(unsigned long long n, unsigned long long confidence) {
 
     unsigned long long q = 1;
     unsigned long long k = 0;
@@ -70,105 +71,131 @@ unsigned long long miller_rabin(int n, int confidence) {
 }
 
 void set_eratostenes_sieve() {
-    int k_prime = 10;
-    for (int j = 0; j < size_initial_sieve; ++j) {
-        for (int k = k_prime + 1; k < 1000; ++k) {
-            if (miller_rabin(k, 5) == 1) {
-                k_prime = k;
-                initial_sieve[j] = k_prime;
-                break;
-            }
-        }
-    }
+  int k_starter = 10;
+  for (int j = 0; j < size_initial_sieve; ++j) {
+      for (int k = k_starter; k < 1000; ++k) {
+          if (miller_rabin(k, 5) == 1) {
+              k_starter = k + 1;
+              initial_sieve[j] = k;
+              break;
+          }
+      }
+  }
 }
 
-int how_many_primes(int n_min_, int n_max, int confidence) {
+unsigned int how_many_primes(unsigned long long n_min_, unsigned long long n_max, unsigned long long confidence) {
 
-    int n_min = n_min_;
-    if (n_min_ < 7)
-        n_min = 7;
+  unsigned long long n_min = n_min_;
+  if (n_min_ < 7)
+      n_min = 7;
 
-    //unsigned long long count_array[omp_get_max_threads()] = {0};
-    unsigned long long count = 0;
-    unsigned long long s_min = static_cast<int>(static_cast<float>(n_min)/6);
-    unsigned long long s_max = static_cast<int>(static_cast<float>(n_max)/6);
+  unsigned long long count = 0;
+  unsigned long long s_min = n_min/6;
+  unsigned long long s_max = n_max/6;
 
+  if (6*s_min - 1 >= n_min) 
+  {
+    if (miller_rabin(6*s_min - 1, confidence) == 1)
+      count += 1;
+  }
+  if (6*s_min + 1 >= n_min)
+  {
     if (miller_rabin(6*s_min + 1, confidence) == 1)
-        count += 1;
+      count += 1;
+  }
 
+  if (6*s_max + 1 < n_max) 
+  {
+    if (miller_rabin(6*s_max + 1, confidence) == 1)
+      count += 1;
+  }
+  if (6*s_max - 1 < n_max)
+  {
     if (miller_rabin(6*s_max - 1, confidence) == 1)
+      count += 1;
+  }
+
+  #pragma omp parallel num_threads(omp_get_max_threads())
+  #pragma omp for reduction(+:count)
+  for (unsigned long long i = s_min + 1; i < s_max; ++i){
+      unsigned long long number = 6*i - 1;
+      
+      for (int j = 0; j < size_initial_sieve; ++j) {
+          if (number%initial_sieve[j] == 0)
+              goto jump2;    
+      }
+
+      if (miller_rabin(number, confidence) == 1)
+      {
         count += 1;
+      }
+      jump2: 1;
+  }
 
-    if (6*s_min - 1 >= n_min) {
-        if (miller_rabin(6*s_min - 1, confidence) == 1)
+  #pragma omp parallel num_threads(omp_get_max_threads())
+  #pragma omp for reduction(+:count)
+  for (unsigned long long i = s_min + 1; i < s_max; ++i){
+      unsigned long long number = 6*i + 1;
+  
+      for (int j = 0; j < size_initial_sieve; ++j) {
+          if (number%initial_sieve[j] == 0)
+              goto jump3;
+      }
+
+      if (miller_rabin(number, confidence) == 1)
+      {
         count += 1;
-    }
+      }
+      jump3: 1;
+  }
+  
+  //for (int j = 0; j < omp_get_max_threads(); j++)
+  //    count += count_array[j];
 
-    if (6*s_max + 1 <= n_max) {
-        if (miller_rabin(6*s_max + 1, confidence) == 1)
-        count += 1;
-    }
+  if (n_min <= 7)
+    return count + size_sufficient_witnesses + size_initial_sieve;
 
-    #pragma omp parallel num_threads(omp_get_max_threads())
-    #pragma omp for reduction(+:count)
-    for (int i = s_min + 1; i < s_max; ++i){
-        unsigned long long number = 6*i - 1;
-        
-        for (int j = 0; j < size_initial_sieve; ++j) {
-            if (number%initial_sieve[j] == 0)
-                goto jump2;    
-        }
-
-        if (miller_rabin(number, confidence) == 1)
-            count += 1;
-        jump2: 1;
-    }
-    
-    #pragma omp parallel num_threads(omp_get_max_threads())
-    #pragma omp for reduction(+:count)
-    for (int i = s_min + 1; i < s_max; ++i){
-        unsigned long long number = 6*i + 1;
-    
-        for (int j = 0; j < size_initial_sieve; ++j) {
-            if (number%initial_sieve[j] == 0)
-                goto jump3;
-        }
-
-        if (miller_rabin(number, confidence) == 1)
-            count += 1;
-        jump3: 1;
-    }
-    
-    //for (int j = 0; j < omp_get_max_threads(); j++)
-    //    count += count_array[j];
-
-    return count + 5 + size_initial_sieve;
+  else 
+    return count;
 }
 
-int main(int argc, char** argv) {
+void continuous_benchmark()
+{
+  unsigned long long n_low = 1;
+  unsigned long long n_high = 1000000;
+  unsigned long long count_total = 0;
+  double number_of_rounds = 1;
+  double average_speed = 0;
+  double std_speed = 0;
 
-    std::ostringstream fn;
-    std::ofstream myfile;
-	  fn << "results.txt";
-	  myfile.open(fn.str());
-
-    int n_min = atoi(argv[1]);
-    int n_max = atoi(argv[2]);
-    int confidence = atoi(argv[3]);
-    
-    set_eratostenes_sieve();
-    
+  while(1)
+  {
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    int count = how_many_primes(n_min, n_max, confidence);
+    int count = how_many_primes(n_low, n_high, 5);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
     long elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
 
-    std::cout << count << " Primes Found\n";
-    std::cout << std::setprecision(3) << std::fixed << static_cast<float>(count)/(static_cast<float>(elapsed_time)*0.000000001)/1000000.0 << " Million Primes/s" << "\n";
+    count_total += count;
+    float speed = static_cast<float>(count)/(static_cast<float>(elapsed_time)*0.000000001)/1000000.0;
+    average_speed += speed;
+    std_speed += speed*speed;
 
-    //myfile << count << "\n";
-    //myfile << static_cast<float>(n_max)/(static_cast<float>(elapsed_time)*0.000000001)/1000000.0 << "\n";
+    std::cout << "\033[2J\033[1;1H";
+    std::cout << "Primes between 1 and " << static_cast<unsigned long long>(n_high/1000000) << " Million ---- " << count_total << "\n";
+    //std::cout << "Instantaneous speed" << std::setprecision(3) << std::fixed << speed << " Million Primes/s" << "\n";
+    std::cout << "Speed " << std::setprecision(3) << std::fixed << average_speed/number_of_rounds << " +- " << sqrt((std_speed - pow(average_speed, 2)/number_of_rounds)/number_of_rounds) <<" Million Primes/s" << "\n";
+    
+    if (n_high == 341550071728321)
+      break;
 
-    myfile.close();
+    n_low = n_high;
+    n_high += 1000000;
+    number_of_rounds += 1;
+  }
+}
+
+int main(int argc, char** argv) 
+{
+    set_eratostenes_sieve();
+    continuous_benchmark();
 }
